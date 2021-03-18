73f3f6488799ff3e9362cf99bc1c3516a162b0a9
wilasineePE <porplabubii@gmail.com>
2021-03-18 18:23:32 +0700

    first commit

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
 import dialogflow_v2 as dialogflow
 import os
-path_key = "C:\wilasinee_pj\pj\python\gthaluangbot-e969e9bc42b3.json"
-os.environ['GOOGLE_APPLICTION_CREDENTIALS'] = path_key
+path_key = "C:\wilasinee_pj\pj\python\ggthaluangbot-a6aed6caf27a.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = path_key
 
 #*****************************************************************
 
 
-project_id = "thaluangbot"
+project_id = "thaluangbot-lhrv"
 sesseion_id = "82d12b78-40b4-4028-a8f7-3a6a479cb2f7"
 language_code = "Thai-th"
 
@@ -25,9 +25,6 @@ import json
 #**************************************
 text = input("let's text : ")
 
-
-
-
 app = Flask(__name__)
 def detect_intents_texts(project_id,sesseion_id,texts,language_code):
     session_client = dialogflow.SessionsClient()
@@ -35,8 +32,8 @@ def detect_intents_texts(project_id,sesseion_id,texts,language_code):
     print('session_path : {}\n'.format(session))
 
     text = texts
-    text_input = dialogflow.types.Text_Input(text=text,language_code=language_code)
-    query_input = dialogflow.types.Query_Input(text = text_input)
+    text_input = dialogflow.types.TextInput(text=text,language_code=language_code)
+    query_input = dialogflow.types.QueryInput(text = text_input)
     response = session_client.detect_intent(session= session,query_input =  query_input)
     return response.query_result.fulfillment_text
 
